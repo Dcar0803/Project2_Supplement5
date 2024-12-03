57e86c9bcdf70d2379c7a801603ecca57b96226d
TwistedWonder2021 <124842528+Dcar0803@users.noreply.github.com>
2024-12-02 22:39:32 -0500

    Function: Create a function for finding the longestpalindromic substring in a given string

---
+++ src/main/java/LongestPalindromicSubstring.java
@@ -0,0 +1,50 @@
+public class LongestPalindromicSubstring {
+    
+	
+	public static String findLongestPalindrome(String s) {
+        
+		
+		if (s == null || s.isEmpty()) {
+			
+            return "";
+            
+        }//end of if statement
+
+        int start = 0, end = 0;
+        
+        for (int i = 0; i < s.length(); i++) {
+            
+        	int len1 = expandAroundCenter(s, i, i);
+            
+        	int len2 = expandAroundCenter(s, i, i + 1);
+           
+        	int len = Math.max(len1, len2);
+            
+        	if (len > end - start) {
+        		
+                start = i - (len - 1) / 2;
+                
+                end = i + len / 2;
+                
+            }//end of if statement
+        	
+        	
+        }//end of for loop
+
+        return s.substring(start, end + 1);
+    }
+
+    private static int expandAroundCenter(String s, int left, int right) {
+        
+    	while (left >= 0 && right < s.length() && s.charAt(left) == s.charAt(right)) {
+            
+    		left--;
+    		
+            right++;
+        }
+    	
+        return right - left - 1;
+        
+    }//end of expandAroundCenter method 
+    
+}//end of class 
