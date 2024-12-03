f0ceac85ce99ddc66b5b5a4db19424b3a57f4d30
TwistedWonder2021 <124842528+Dcar0803@users.noreply.github.com>
2024-12-02 21:58:58 -0500

    Test: Created test function fir determing if a 2D matrix is a magic square

--- src/test/java/Testing.java
+++ src/test/java/Testing.java
@@ -5,8 +5,22 @@ import org.junit.jupiter.api.Test;
 class Testing {
 
 	@Test
-	void test() {
-		fail("Not yet implemented");
-	}
+    public void testIsMagicSquare() {
+        int[][] magicSquare = {
+            {2, 7, 6},
+            {9, 5, 1},
+            {4, 3, 8}
+        };
 
-}
+        int[][] nonMagicSquare = {
+            {1, 2, 3},
+            {4, 5, 6},
+            {7, 8, 9}
+        };
+
+        assertTrue(MagicSquare.isMagicSquare(magicSquare));
+        assertFalse(MagicSquare.isMagicSquare(nonMagicSquare));
+    
+	}//end of testIsMagicSquare 
+
+}//end of class
